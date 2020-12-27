1f19a79c128bf5e2f1978cc88874580dfda4a63b
zhaozigu <zhaozigunan@163.com>
2020-12-27 17:03:26 +0800

    最终游戏

--- BasicPhysics/Ship.cpp
+++ BasicPhysics/Ship.cpp
@@ -8,9 +8,11 @@
 
 #include "Ship.hpp"
 #include "Game.hpp"
+#include "Laser.hpp"
 
 Ship::Ship(Game* game)
 : Actor(game)
+, mLaserCooldown(0.0f)
 , sc(new SpriteComponent(this, 150))
 , ic(new InputComponent(this))
 {
@@ -27,7 +29,7 @@ Ship::Ship(Game* game)
 
 void Ship::UpdateActor(float deltaTime)
 {
-    
+    mLaserCooldown -= deltaTime;
 }
 
 Ship::~Ship()
@@ -38,5 +40,14 @@ Ship::~Ship()
 
 void Ship::ActorInput(const uint8_t* keyState)
 {
-    
+    if (keyState[SDL_SCANCODE_SPACE] && mLaserCooldown <= 0.0f)
+    {
+        // 创建激光
+        Laser* laser = new Laser(GetGame());
+        laser->SetPosition(GetPosition());
+        laser->SetRotation(GetRotation());
+
+        // 设定冷却期
+        mLaserCooldown = 0.5f;
+    }
 }
--- BasicPhysics/Ship.hpp
+++ BasicPhysics/Ship.hpp
@@ -21,6 +21,8 @@ public:
     void UpdateActor(float deltaTime) override;
     void ActorInput(const uint8_t* keyState) override;
 private:
+    float mLaserCooldown;
+    
     SpriteComponent* sc;
     InputComponent* ic;
 };
